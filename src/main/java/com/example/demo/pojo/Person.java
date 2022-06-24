124225073d6baadfe90a724a02784c9c64d463eb
wang <768551676@qq.com>
2022-06-24 15:38:32 +0800

    yaml 的配置方式 @ConfigurationProperties @PropertySource

--- src/main/java/com/example/demo/pojo/Person.java
+++ src/main/java/com/example/demo/pojo/Person.java
@@ -1,5 +1,7 @@
 package com.example.demo.pojo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
@@ -9,10 +11,11 @@ import java.util.Map;
 
 @Component
 @ConfigurationProperties(prefix = "person")
+//@PropertySource(value = "classpath:person.properties")
 @Validated //数据校验JSR303 校验
-
 public class Person {
 
+    @Value("${name}")
     private String name;
     private Integer age;
     private Boolean happy;
